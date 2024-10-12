7aab01859f37c7342c4e5237b5be078520568cc2
Nikol <1nikol.otahalova@gmail.com>
2024-10-12 21:56:39 +0200

    Created MenuBar without Functions, yet with Placeholder

--- src/main/java/cz/muni/fi/pv168/project/ui/MainWindow.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/MainWindow.java
@@ -9,18 +9,17 @@ import java.awt.Color;
 public class MainWindow {
 
     private final JFrame frame;
-
     public MainWindow() {
         frame = this.createFrame();
     }
 
     private JFrame createFrame() {
         JFrame frame = new JFrame("MIB Task Manager");
-
         frame.setResizable(false);
         frame.setSize(1000, 600);
         frame.getContentPane().setBackground(new Color(180, 180, 180));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setJMenuBar(createMenuBar());
         return frame;
     }
 
@@ -28,4 +27,26 @@ public class MainWindow {
         frame.setVisible(true);
     }
 
+    private JMenuBar createMenuBar(){
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBackground(new Color(240, 240, 240));
+
+        menuBar.add(createJMenu("File"));
+        menuBar.add(createJMenu("Template"));
+        menuBar.add((createJMenu("Categories")));
+        menuBar.add((createJMenu("Time Units")));
+        menuBar.add(createJMenu("Help"));
+
+        return  menuBar;
+
+    }
+    private JMenu createJMenu(String name, Action ... actionList)
+    {
+        JMenu menu = new JMenu(name);
+        menu.add("PLACEHOLDER_ACTION");
+        for (Action a: actionList) {
+            menu.add(a.toString());
+        }
+        return menu;
+    }
 }
