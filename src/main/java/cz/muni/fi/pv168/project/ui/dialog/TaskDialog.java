2f9596c195bcabc5dccacead9f78ca96f5e94249
Marcel Nadzam <xnadzam@fi.muni.cz>
2024-10-20 17:10:29 +0200

    fix categories not working + begin work on templates

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/TaskDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/TaskDialog.java
@@ -7,6 +7,8 @@ import cz.muni.fi.pv168.project.model.CustomTimeUnit;
 import cz.muni.fi.pv168.project.model.Status;
 import cz.muni.fi.pv168.project.model.Task;
 import cz.muni.fi.pv168.project.model.TimeUnit;
+import cz.muni.fi.pv168.project.ui.model.CategoryListModel;
+import cz.muni.fi.pv168.project.ui.model.TimeUnitListModel;
 
 import javax.swing.*;
 
@@ -19,8 +21,8 @@ public class TaskDialog extends EntityDialog<Task>{
 
     private final JComboBox<Status> statusComboBox = new JComboBox<>(Status.values());
 
-    private final Object[] categories;
     private final JComboBox<Object> categoryComboBox;
+    private final JComboBox<TimeUnit> timeUnitComboBox;
 
     private final JIntegerTextField loggedTimeField = new JIntegerTextField();
     private final JIntegerTextField allocatedTimeField = new JIntegerTextField();
@@ -29,49 +31,65 @@ public class TaskDialog extends EntityDialog<Task>{
     //TODO add combobox for time units
     private final TimeUnit timeUnit = new CustomTimeUnit();
 
-    public TaskDialog(Task task, Object[] categories) {
+    public TaskDialog(Task task, CategoryListModel categories, TimeUnitListModel timeUnits) {
         this.task = task;
-        this.categories = categories;
-        this.categoryComboBox = new JComboBox<>(categories);
-        setValues();
+        this.categoryComboBox = new JComboBox<>(categories.toArray());
+        this.timeUnitComboBox = new JComboBox<>(timeUnits.toArray());
+        if (task != null) {
+            setValues(task);
+        }
         addFields();
         setPanel();
     }
 
-    private void setValues()
+    private void setValues(Task task)
     {
-        taskNameField.setText(task.getNameOfTask());
-        customerField.setText(task.getCustomer());
-        categoryComboBox.setSelectedItem(task.getCategory());
-        statusComboBox.setSelectedItem(task.getCategory());
-        loggedTimeField.setValue(task.getLoggedTime());
-        allocatedTimeField.setValue(task.getAllocatedTime());
-        datePicker.setDate(task.getDueDate());
-        timeUnit.setName(task.getTimeUnit().getName());
-        timeUnit.setRate(task.getTimeUnit().getRate());
+        taskNameField.setText(this.task.getNameOfTask());
+        customerField.setText(this.task.getCustomer());
+        categoryComboBox.setSelectedItem(this.task.getCategory());
+        statusComboBox.setSelectedItem(this.task.getCategory());
+        loggedTimeField.setValue(this.task.getLoggedTime());
+        allocatedTimeField.setValue(this.task.getAllocatedTime());
+        datePicker.setDate(this.task.getDueDate());
+        timeUnit.setName(this.task.getTimeUnit().getName());
+        timeUnit.setRate(this.task.getTimeUnit().getRate());
     }
 
     private void addFields(){
         add("Task name", taskNameField);
         add("Customer", customerField);
-        add("Category", new JComboBox<>(categories));
+        add("Category", categoryComboBox);
         add("Status", statusComboBox);
         add("Logged time", loggedTimeField);
         add("Allocated time", allocatedTimeField);
-        add("Time unit", new JLabel(task.getTimeUnit().getName()));
+        add("Time unit", timeUnitComboBox);
         add("Due date", datePicker);
 
     }
 
     @Override
     Task getEntity() {
-        task.setNameOfTask(taskNameField.getText());
-        task.setCustomer(customerField.getText());
-        task.setCategory((Category) categoryComboBox.getSelectedItem());
-        task.setStatus((Status) statusComboBox.getSelectedItem());
-        task.setLoggedTime(loggedTimeField.getValue());
-        task.setAllocatedTime(allocatedTimeField.getValue());
-        task.setDueDate(datePicker.getDate());
+        var task = this.task;
+        if (task != null) {
+            task.setNameOfTask(taskNameField.getText());
+            task.setCustomer(customerField.getText());
+            task.setCategory((Category) categoryComboBox.getSelectedItem());
+            task.setStatus((Status) statusComboBox.getSelectedItem());
+            task.setLoggedTime(loggedTimeField.getValue());
+            task.setAllocatedTime(allocatedTimeField.getValue());
+            task.setDueDate(datePicker.getDate());
+            task.setTimeUnit((TimeUnit) timeUnitComboBox.getSelectedItem());
+        } else {
+            task = new Task((Status) statusComboBox.getSelectedItem(),
+                    (Category) categoryComboBox.getSelectedItem(),
+                    customerField.getText(),
+                    taskNameField.getText(),
+                    "TODO",
+                    loggedTimeField.getValue(),
+                    allocatedTimeField.getValue(),
+                    (TimeUnit) timeUnitComboBox.getSelectedItem(),
+                    datePicker.getDate());
+        }
         return task;
     }
 }
