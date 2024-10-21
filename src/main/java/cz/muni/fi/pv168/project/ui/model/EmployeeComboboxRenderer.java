f74090c56f35075cca82f63bbaa32845662b9c75
Maroš Pavlík <xpavlik9@fi.muni.cz>
2024-10-21 14:55:35 +0200

    GUI - task-assign-to complete

---
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/EmployeeComboboxRenderer.java
@@ -0,0 +1,23 @@
+package cz.muni.fi.pv168.project.ui.model;
+
+import cz.muni.fi.pv168.project.model.Employee;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Renderer for combobox of {@link Employee}, it displays the id and the name.
+ */
+public class EmployeeComboboxRenderer extends DefaultListCellRenderer {
+
+    @Override
+    public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+
+        if (value instanceof Employee employee) {
+            label.setText(employee.getCode());
+        }
+
+        return label;
+    }
+}
