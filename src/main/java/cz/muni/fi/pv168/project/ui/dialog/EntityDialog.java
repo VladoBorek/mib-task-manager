8facb42cbaab6c86d79b92bf5af3431a9c13bf7c
Vladimír Borek <xborek@fi.muni.cz>
2024-10-21 18:00:36 +0200

    deleted pointless comments in entityDialog

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/EntityDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/EntityDialog.java
@@ -21,25 +21,15 @@ abstract class EntityDialog<E> {
         componentPanel.setLayout(new GridLayout(0, 1));
     }
     EntityDialog(int width, int height) {
-//        var layout = new BoxLayout(panel, BoxLayout.X_AXIS);
-//        panel.setLayout(layout);
-//
-//        labelPanel.setLayout(new GridLayout(0, 1));
-//        componentPanel.setLayout(new GridLayout(0, 1));
-//
-//        panel.setPreferredSize(new Dimension(width, height));
-
         panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 
-        labelPanel.setLayout(new GridLayout(0, 1));  // Labels in a single column
-        componentPanel.setLayout(new GridLayout(0, 1));  // Components in a single column
-        //buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));  // Buttons as needed
-        buttonPanel.setLayout(new GridLayout(0, 1));  // Buttons as needed
+        labelPanel.setLayout(new GridLayout(0, 1));
+        componentPanel.setLayout(new GridLayout(0, 1));
+        buttonPanel.setLayout(new GridLayout(0, 1));
 
-        // Add panels to main panel
-        panel.add(labelPanel);         // First column (labels)
-        panel.add(componentPanel);     // Second column (components)
-        panel.add(buttonPanel);        // Third column (buttons)
+        panel.add(labelPanel);
+        panel.add(componentPanel);
+        panel.add(buttonPanel);
 
         panel.setPreferredSize(new Dimension(width, height));
     }
@@ -54,17 +44,6 @@ abstract class EntityDialog<E> {
 
     void addCentered(String labelText, JComponent component) {
         addCentered(labelText, component, null);
-//        var label = new JLabel(labelText);
-//        label.setHorizontalAlignment(SwingConstants.CENTER);
-//        labelPanel.add(label);
-//
-//        JPanel componentWrapper = new JPanel();
-//        componentWrapper.setLayout(new FlowLayout(FlowLayout.CENTER));
-//        componentWrapper.add(component);
-//        componentPanel.add(component, "wmin 250lp, grow");
-
-        //componentPanel.add(Box.createHorizontalStrut(0));
-
     }
 
     void addCentered(String labelText, JComponent component, JButton  button) {
@@ -76,10 +55,9 @@ abstract class EntityDialog<E> {
         componentWrapper.setLayout(new FlowLayout(FlowLayout.CENTER));
         componentWrapper.add(component);
         componentPanel.add(component, "wmin 250lp, grow");
-        //componentPanel.add(button);
 
         if (button != null) {
-            buttonPanel.add(button); // Add the button to the buttonPanel
+            buttonPanel.add(button);
         }
         else {
             buttonPanel.add(new JLabel(""));
