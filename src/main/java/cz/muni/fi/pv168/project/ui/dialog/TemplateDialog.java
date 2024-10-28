872f85d127a7de32af9cb9c67431f488a5663469
Maroš Pavlík <xpavlik9@fi.muni.cz>
2024-10-28 14:34:01 +0100

    category colors fix

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/TemplateDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/TemplateDialog.java
@@ -6,6 +6,7 @@ import cz.muni.fi.pv168.project.business.model.CustomTimeUnit;
 import cz.muni.fi.pv168.project.business.model.DataManager;
 import cz.muni.fi.pv168.project.business.model.Template;
 import cz.muni.fi.pv168.project.business.model.TimeUnit;
+import cz.muni.fi.pv168.project.ui.model.CategoryComboboxRenderer;
 
 import javax.swing.*;
 
@@ -24,6 +25,13 @@ public class TemplateDialog extends EntityDialog<Template>{
         this.data = data;
         this.timeUnitComboBox = new JComboBox<>(data.getTimeUnits().toArray());
         this.categoryComboBox = new JComboBox<>(data.getCategories().toArray());
+
+        categoryComboBox.setRenderer(new CategoryComboboxRenderer());
+        categoryComboBox.addActionListener(e -> {
+            CategoryComboboxRenderer.setCategoryComboboxColor(categoryComboBox);
+        });
+        CategoryComboboxRenderer.setCategoryComboboxColor(categoryComboBox);
+
         this.template = template;
         if (template != null) {
             setValues();
@@ -48,7 +56,7 @@ public class TemplateDialog extends EntityDialog<Template>{
     private void addFields(){
         add("Template name", templateNameField);
         add("Task name", nameField);
-        add("Category", new JComboBox<>(data.getCategories().toArray()));
+        add("Category", categoryComboBox);
         add("Allocated time", allocatedTimeField);
         add("Time unit", timeUnitComboBox);
 
