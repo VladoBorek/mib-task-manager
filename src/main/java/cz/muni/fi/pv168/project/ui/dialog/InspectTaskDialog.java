32705befb4d19d61364ca5766ca531ce90c3273d
Vladimír Borek <xborek@fi.muni.cz>
2024-11-06 21:07:54 +0100

    refactored: inspect task dialog

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/InspectTaskDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/InspectTaskDialog.java
@@ -23,51 +23,38 @@ import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
-public class InspectTaskDialog extends EntityDialog<Task>{
+public class InspectTaskDialog extends EntityDialog<Task> {
 
     private static final Border labelBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
     private final Task task;
     private final JLabel taskName = new JLabel();
-    private final JTextArea  description = new JTextArea();
+    private final JTextArea description = new JTextArea();
     private final JLabel customer = new JLabel();
     private final JLabel assignedTo = new JLabel();
     private final JLabel status = new JLabel();
-    private JTable timeLogTable;
+    private final JTable timeLogTable;
     private final JLabel category = new JLabel();
     private final JLabel loggedTime = new JLabel();
     private final JLabel allocatedTime = new JLabel();
     private final JLabel date = new JLabel();
-
     private final DataManager data;
-
     private final JPanel leftPanel = new JPanel();
     private final JPanel rightPanel = new JPanel();
-    private final JPanel logTablePanel = new JPanel();
-    private final JPanel logBottomInfoPanel = new JPanel();
 
+    public InspectTaskDialog(Task task, DataManager data) {
+        super(700, 400);
 
- // *** CALE TOTO ESTE REFACTORNEM
-
-    public InspectTaskDialog(Task task, DataManager data){
-        super(700, 400, ActionType.INSPECT);
         this.data = data;
         this.task = task;
         this.timeLogTable = task.getTimeLogTable();
 
-        description.setPreferredSize(new Dimension(200, 100));
-        description.setMinimumSize(new Dimension(200, 100));
-        description.setMaximumSize(new Dimension(200, 100));
-
         setValues();
-        SetupPanelLayouts();
-        //setValues();
-        //addFields();
-        //setPanel();
         FormatFields();
+        SetupPanels();
     }
 
-    private void SetupPanelLayouts(){
-        //leftPanel.setLayout(new GridLayout(0, 2));
+    private void SetupPanels() {
+        super.getPanel().setLayout(new GridLayout(1, 2));
         leftPanel.setLayout(new BorderLayout());
         rightPanel.setLayout(new BorderLayout());
 
@@ -80,50 +67,55 @@ public class InspectTaskDialog extends EntityDialog<Task>{
         super.getPanel().add(leftPanel);
         super.getPanel().add(rightPanel);
 
-        JPanel infoLabelsPanel = new JPanel(new GridLayout(3, 2));
-        JPanel descriptionLabelPanel = new JPanel(new BorderLayout());
+        leftPanel.add(setupInfoPanel());
+        leftPanel.add(setupDescriptionPanel(), BorderLayout.SOUTH);
 
-        JPanel textLabelPanel = new JPanel(new BorderLayout());
-        JLabel textLabel = new JLabel("Description:");
-        textLabelPanel.add(textLabel);
-        //textLabelPanel.setBackground(Color.YELLOW);
+        rightPanel.add(setupLogTablePanel(), BorderLayout.CENTER);
+        rightPanel.add(setupBottomPanel(), BorderLayout.SOUTH);
+    }
 
-        JPanel labelPanel = new JPanel(new BorderLayout());
-        labelPanel.add(description);
+    private JPanel setupBottomPanel() {
+        JPanel bottomPanel = new JPanel(new BorderLayout());
 
-        //labelPanel.setBackground(Color.GREEN);
+        bottomPanel.add(setupTimeTitlesPanel(), BorderLayout.NORTH);
+        bottomPanel.add(setupTimeInfoPanel(), BorderLayout.SOUTH);
 
-        descriptionLabelPanel.add(textLabelPanel, BorderLayout.NORTH);
-        descriptionLabelPanel.add(labelPanel, BorderLayout.CENTER);
+        return bottomPanel;
+    }
 
-        // infoLabelsPanel.setBackground(Color.WHITE);
-        //descriptionLabelPanel.setBackground(Color.BLUE);
+    private JPanel setupTimeTitlesPanel() {
+        JPanel timeTitlesPanel = new JPanel(new GridLayout(1, 3));
 
+        timeTitlesPanel.add(new JLabel("Allocated time", SwingConstants.CENTER));
+        timeTitlesPanel.add(new JLabel("Total logged", SwingConstants.CENTER));
+        timeTitlesPanel.add(new JLabel("Log time", SwingConstants.CENTER));
 
-        leftPanel.add(infoLabelsPanel);
-        leftPanel.add(descriptionLabelPanel,BorderLayout.SOUTH);
+        return timeTitlesPanel;
+    }
 
-        //descriptionLabelPanel.add(description);
+    private JPanel setupTimeInfoPanel() {
+        JPanel timeInfoPanel = new JPanel();
 
-        infoLabelsPanel.add(new CellPanel("Task-name:", taskName));
-        infoLabelsPanel.add(new CellPanel("Customer:", customer));
-        infoLabelsPanel.add(new CellPanel("Category:", category));
-        infoLabelsPanel.add(new CellPanel("Assigned to:", assignedTo));
-        infoLabelsPanel.add(new CellPanel("Status:", status));
-        infoLabelsPanel.add(new CellPanel("Due date:", date));
+        timeInfoPanel.setLayout(new GridLayout(1, 3));
+        JButton addLogTimeButton = MainWindow.createButton("", Icons.ADD_ICON,
+                new LogTimeAction(data, this, task));
 
+        allocatedTime.setPreferredSize(new Dimension(200, 80));
+        loggedTime.setPreferredSize(new Dimension(200, 80));
+        addLogTimeButton.setPreferredSize(new Dimension(200, 80));
 
-        // *** RIGHT SIDE *** //
+        timeInfoPanel.add(this.allocatedTime);
+        timeInfoPanel.add(this.loggedTime);
+        timeInfoPanel.add(addLogTimeButton);
 
+        return timeInfoPanel;
+    }
 
-        //leftPanel.add(super.getLabelPanel());
-        //leftPanel.add(super.getComponentPanel());
-
-        //logTablePanel.setBackground(Color.DARK_GRAY);
+    private JPanel setupLogTablePanel() {
+        JPanel logTablePanel = new JPanel();
         logTablePanel.setBackground(new Color(211, 211, 211));
         logTablePanel.setPreferredSize(new Dimension(300, 250));
         logTablePanel.add(new JLabel("LOG TIME TABLE"));
-
 
         JScrollPane scrollPane = new JScrollPane(timeLogTable);
         scrollPane.setPreferredSize(new Dimension(300, 250));
@@ -132,56 +124,52 @@ public class InspectTaskDialog extends EntityDialog<Task>{
         componentWrapper.add(scrollPane);
 
         logTablePanel.add(scrollPane);
-
-
-
-        JPanel timeLogtextLabelsPanel = new JPanel(new GridLayout(1, 3));
-
-        JLabel allocatedTimeLabel = new JLabel("Allocated time", SwingConstants.CENTER);
-        JLabel totalTimeLabel = new JLabel("Total logged", SwingConstants.CENTER);
-        JLabel logTimeLabel = new JLabel("Log time", SwingConstants.CENTER);
-
-        timeLogtextLabelsPanel.add(allocatedTimeLabel);
-        timeLogtextLabelsPanel.add(totalTimeLabel);
-        timeLogtextLabelsPanel.add(logTimeLabel);
-
-        logBottomInfoPanel.setLayout(new GridLayout(1, 3));
-
-        //JButton logButton = new JButton("Log 10h");
-        // JButton allocatedButton = new JButton("Al 5h");
-        JButton addLogTimeButton = MainWindow.createButton("", Icons.ADD_ICON,
-                new LogTimeAction(data, this, task));
-
-        allocatedTime.setPreferredSize(new Dimension(200, 80));
-        loggedTime.setPreferredSize(new Dimension(200, 80));
-        addLogTimeButton.setPreferredSize(new Dimension(200, 80));
-
-        //this.allocatedTime.setPreferredSize(new Dimension(200, 80));
-
-        logBottomInfoPanel.add(this.allocatedTime);
-        logBottomInfoPanel.add(this.loggedTime);
-        logBottomInfoPanel.add(addLogTimeButton);
-
-
-        JPanel bottomPanel = new JPanel(new BorderLayout());
-        bottomPanel.add(timeLogtextLabelsPanel, BorderLayout.NORTH);
-        bottomPanel.add(logBottomInfoPanel, BorderLayout.SOUTH);
-
-        rightPanel.add(logTablePanel, BorderLayout.CENTER);
-        rightPanel.add(bottomPanel, BorderLayout.SOUTH);
-
+        return logTablePanel;
     }
-    private void FormatFields(){
-        centerOutText();
-        setBorders();
+
+    private JPanel setupInfoPanel() {
+        JPanel infoLabelsPanel = new JPanel(new GridLayout(3, 2));
+
+        infoLabelsPanel.add(new CellPanel("Task-name:", taskName));
+        infoLabelsPanel.add(new CellPanel("Customer:", customer));
+        infoLabelsPanel.add(new CellPanel("Category:", category));
+        infoLabelsPanel.add(new CellPanel("Assigned to:", assignedTo));
+        infoLabelsPanel.add(new CellPanel("Status:", status));
+        infoLabelsPanel.add(new CellPanel("Due date:", date));
+
+        return infoLabelsPanel;
+    }
+
+    private JPanel setupDescriptionPanel() {
+        JPanel descriptionLabelPanel = new JPanel(new BorderLayout());
+
+        JPanel titleDescriptionPanel = new JPanel(new BorderLayout());
+        titleDescriptionPanel.add(new JLabel("Description:"));
+
+        JPanel textDescriptionPanel = new JPanel(new BorderLayout());
+        textDescriptionPanel.add(description);
+
+        descriptionLabelPanel.add(titleDescriptionPanel, BorderLayout.NORTH);
+        descriptionLabelPanel.add(textDescriptionPanel, BorderLayout.CENTER);
+
+        description.setPreferredSize(new Dimension(200, 100));
+        description.setMinimumSize(new Dimension(200, 100));
+        description.setMaximumSize(new Dimension(200, 100));
 
         description.setLineWrap(true);
         description.setWrapStyleWord(true);
         description.setEditable(false);
         description.setOpaque(false);
+
+        return descriptionLabelPanel;
     }
 
-    private void setBorders(){
+    private void FormatFields() {
+        centerOutText();
+        setBorders();
+    }
+
+    private void setBorders() {
         taskName.setBorder(labelBorder);
         customer.setBorder(labelBorder);
         assignedTo.setBorder(labelBorder);
@@ -211,44 +199,16 @@ public class InspectTaskDialog extends EntityDialog<Task>{
         customer.setText(task.getCustomer());
         assignedTo.setText(task.getAssignedTo().toString());
         category.setText(task.getCategory().getName());
-
-//        category.setBackground(task.getCategory().getColor());  \\TODO does not work
-//        category.setForeground(CategoryComboboxRenderer.getRightTextColor(task.getCategory().getColor()));
-//        System.out.println(task.getCategory().getColor().toString());
-
         status.setText(task.getStatus().toString());
         loggedTime.setText(task.getConvertedLoggedTimeString());
         allocatedTime.setText(task.getConvertedAllocatedTimeString());
         date.setText(task.getDueDate().toString());
     }
 
-    private void addFields(){
-        JButton addLogTimeButton = MainWindow.createButton("", Icons.ADD_ICON,
-                new LogTimeAction(data, this, task));
-
-        addCentered("Task name", taskName);
-        addCentered("Description", description);
-        addCentered("Customer", customer);
-        addCentered("Category", category);
-        addCentered("Assigned to", assignedTo);
-        addCentered("Status", status);
-
-//        JScrollPane scrollPane = new JScrollPane(timeLogTable);
-//        scrollPane.setPreferredSize(new Dimension(200, 100));
-//        addCentered("Time log table", scrollPane);
-
-        //addCentered("Total logged time", loggedTime, addLogTimeButton);
-        //addCentered("Allocated time", allocatedTime);
-        addCentered("Due date", date);
-
-
-
-
-    }
-
-    public void updateLoggedTime(){
+    public void updateLoggedTime() {
         loggedTime.setText(task.getConvertedLoggedTimeString());
     }
+
     @Override
     Task getEntity() {
         return task;
