a94d3d3cc5419032735e5e19697b9df62a7c03ef
Maroš Pavlík <xpavlik9@fi.muni.cz>
2024-11-22 18:33:26 +0100

    FIX - make User record

--- src/main/java/cz/muni/fi/pv168/project/business/model/User.java
+++ src/main/java/cz/muni/fi/pv168/project/business/model/User.java
@@ -1,23 +1,4 @@
 package cz.muni.fi.pv168.project.business.model;
 
-import cz.muni.fi.pv168.project.business.model.abstracts.Entity;
-
-public class User extends Entity {
-    private final String username;
-    private final Long Id;
-
-    public User(String username, Long id) {
-        super(id);
-        this.username = username;
-        this.Id = id;
-    }
-
-    public Long getId(){
-        return this.Id;
-    }
-
-    public String getUsername(){
-        return this.username;
-    }
-}
+public record User(String username, Long id){}
 
