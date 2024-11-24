01954872ff96296b3dc33197e26e6992c770c4ef
Vladimír Borek <xborek@fi.muni.cz>
2024-11-24 23:13:53 +0100

    reworked statistics + added new statistic

--- src/main/java/cz/muni/fi/pv168/project/ui/model/storagemodels/StatisticsTableModel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/storagemodels/StatisticsTableModel.java
@@ -1,91 +1,41 @@
 package cz.muni.fi.pv168.project.ui.model.storagemodels;
 
 import cz.muni.fi.pv168.project.business.model.Statistic;
-import cz.muni.fi.pv168.project.business.model.Status;
+import cz.muni.fi.pv168.project.business.service.Statistics.StatisticsService;
 import cz.muni.fi.pv168.project.ui.DataManager;
 import cz.muni.fi.pv168.project.ui.model.statisticTableCell;
 
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableRowSorter;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
+import static cz.muni.fi.pv168.project.business.service.Statistics.StatisticsService.formatLoggedAllocatedTime;
 
 /**
  * @author Vladimir Borek
  */
 public class StatisticsTableModel extends AbstractTableModel {
-    private static final int STATUS_INDEX = 1;
-    private static final int DUEDATE_INDEX = 8;
     private final String[] columnNames = {"Statistic Name", "Global Statistic", "Filtered Statistic"};
-    private Statistic globalStatistics = new Statistic();
-    private Statistic filteredStatistics = new Statistic();
-    private List<statisticTableCell> statisticsData;
+    private final List<statisticTableCell> statisticsData = new ArrayList<>();
     private final DataManager data;
+    private Statistic globalStatistics;
+    private Statistic filteredStatistics;
 
     public StatisticsTableModel(DataManager data) {
-        this.statisticsData = new ArrayList<>();
         this.data = data;
-
-        calculateGlobalStatistics();
-        calculateFilteredStatistics();
-        addStatisticsToTable();
+        refreshStatistics();
     }
 
     private void addStatisticsToTable() {
-        statisticsData.add(new statisticTableCell(
-                "Total Tasks", globalStatistics.getTotal(), filteredStatistics.getTotal()));
-        statisticsData.add(new statisticTableCell(
-                "Completed Tasks", globalStatistics.getCompleted(), filteredStatistics.getCompleted()));
-        statisticsData.add(new statisticTableCell(
-                "Overdue Tasks", globalStatistics.getOverdue(), filteredStatistics.getOverdue()));
-        statisticsData.add(new statisticTableCell(
-                "Unfinished Tasks", globalStatistics.getInProgress(), filteredStatistics.getInProgress()));
-    }
-
-    private void calculateGlobalStatistics() {
-        TaskTableModel taskTableModel = (TaskTableModel) data.getTaskTable().getModel();
-        int globalTotalTasks = taskTableModel.getRowCount();
-        globalStatistics.setTotal(globalTotalTasks);
-
-        for (int i = 0; i < taskTableModel.getRowCount(); i++) {
-            Status taskStatus = (Status) taskTableModel.getValueAt(i, STATUS_INDEX);
-            if (taskStatus == Status.COMPLETED) {
-                globalStatistics.setCompleted(globalStatistics.getCompleted() + 1);
-
-            } else {
-                globalStatistics.setInProgress(globalStatistics.getInProgress() + 1);
-            }
-
-            LocalDate taskDueDate = (LocalDate) taskTableModel.getValueAt(i, DUEDATE_INDEX);
-            if (LocalDate.now().isAfter(taskDueDate)) {
-                globalStatistics.setOverdue(globalStatistics.getOverdue() + 1);
-            }
-        }
-    }
-
-    private void calculateFilteredStatistics() {
-        TaskTableModel taskTableModel = (TaskTableModel) data.getTaskTable().getModel();
-        TableRowSorter<TaskTableModel> rowSorter = (TableRowSorter<TaskTableModel>) data.getTaskTable().getRowSorter();
-
-        int filteredTotalTasks = rowSorter.getViewRowCount();
-        filteredStatistics.setTotal(filteredTotalTasks);
-
-        for (int i = 0; i < filteredTotalTasks; i++) {
-            int modelIndex = rowSorter.convertRowIndexToModel(i);
-
-            Status taskStatus = (Status) taskTableModel.getValueAt(modelIndex, STATUS_INDEX);
-            if (taskStatus == Status.COMPLETED) {
-                filteredStatistics.setCompleted(filteredStatistics.getCompleted() + 1);
-            } else {
-                filteredStatistics.setInProgress(filteredStatistics.getInProgress() + 1);
-            }
-
-            LocalDate taskDueDate = (LocalDate) taskTableModel.getValueAt(modelIndex, DUEDATE_INDEX);
-            if (LocalDate.now().isAfter(taskDueDate)) {
-                filteredStatistics.setOverdue(filteredStatistics.getOverdue() + 1);
-            }
-        }
+        statisticsData.clear();
+        statisticsData.add(new statisticTableCell("Total Tasks", globalStatistics.total(), filteredStatistics.total()));
+        statisticsData.add(new statisticTableCell("Completed Tasks", globalStatistics.completed(), filteredStatistics.completed()));
+        statisticsData.add(new statisticTableCell("Overdue Tasks", globalStatistics.overdue(), filteredStatistics.overdue()));
+        statisticsData.add(new statisticTableCell("Unfinished Tasks", globalStatistics.inProgress(), filteredStatistics.inProgress()));
+        statisticsData.add(new statisticTableCell("Logged Time / Allocated Time",
+                formatLoggedAllocatedTime(globalStatistics), formatLoggedAllocatedTime(filteredStatistics))
+        );
     }
 
     @Override
@@ -115,12 +65,11 @@ public class StatisticsTableModel extends AbstractTableModel {
     }
 
     public void refreshStatistics() {
-        globalStatistics = new Statistic();
-        filteredStatistics = new Statistic();
-        statisticsData = new ArrayList<>();
+        var taskTableModel = (TaskTableModel) data.getTaskTable().getModel();
+        var rowSorter = (TableRowSorter<TaskTableModel>) data.getTaskTable().getRowSorter();
 
-        calculateGlobalStatistics();
-        calculateFilteredStatistics();
+        globalStatistics = StatisticsService.calculateGlobalStatistics(taskTableModel);
+        filteredStatistics = StatisticsService.calculateFilteredStatistics(taskTableModel, rowSorter);
 
         addStatisticsToTable();
         fireTableDataChanged();
