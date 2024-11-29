801ac2ead9ecbf9b8065c6c4f4415e41a3651bf4
Nikol Otáhalů <xotahal2@fi.muni.cz>
2024-11-29 10:18:01 +0100

    Added export/import of template description and assigned to, added validation of template description and assigned to, fixed formatting of task table

--- src/main/java/cz/muni/fi/pv168/project/ui/model/storagemodels/TaskTableModel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/storagemodels/TaskTableModel.java
@@ -13,15 +13,15 @@ import java.util.List;
 
 public class TaskTableModel extends BaseTableModel<Task> implements EntityTableModel<Task> {
     private final List<Column<Task, ?>> columns = List.of(
-            Column.readonly("TASK NAME", String.class, Task::getName),
-            Column.readonly("STATUS", Status.class, Task::getStatus),
-            Column.readonly("CATEGORY", Category.class, Task::getCategory),
-            Column.readonly("CUSTOMER", String.class, Task::getCustomer),
-            Column.readonly("ASSIGNED TO", String.class, Task::getAssignedTo),
-            Column.readonly("LOGGED TIME", String.class, Task::getConvertedLoggedTimeString),
-            Column.readonly("ALLOCATED TIME", String.class, Task::getConvertedAllocatedTimeString),
-            Column.readonly("% DONE", Float.class, Task::getPercentage),
-            Column.readonly("DUE DATE", LocalDate.class, Task::getDueDate)
+            Column.readonly("Task Name", String.class, Task::getName),
+            Column.readonly("Status", Status.class, Task::getStatus),
+            Column.readonly("Category", Category.class, Task::getCategory),
+            Column.readonly("Customer", String.class, Task::getCustomer),
+            Column.readonly("Assigned to", String.class, Task::getAssignedTo),
+            Column.readonly("Logged time", String.class, Task::getConvertedLoggedTimeString),
+            Column.readonly("Allocated time", String.class, Task::getConvertedAllocatedTimeString),
+            Column.readonly("% done", Float.class, Task::getPercentage),
+            Column.readonly("Due date", LocalDate.class, Task::getDueDate)
     );
 
     public TaskTableModel(CrudService<Task> taskCrudService) {
