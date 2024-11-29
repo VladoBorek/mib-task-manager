73e1eed29f88075aa9e0fd95cc9b70ae3f91cb73
Nikol Otáhalů <xotahal2@fi.muni.cz>
2024-11-29 10:06:00 +0100

    Removed unnecessary auto-import

--- src/main/java/cz/muni/fi/pv168/project/business/service/validation/TemplateValidator.java
+++ src/main/java/cz/muni/fi/pv168/project/business/service/validation/TemplateValidator.java
@@ -1,6 +1,5 @@
 package cz.muni.fi.pv168.project.business.service.validation;
 
-import cz.muni.fi.pv168.project.business.model.Task;
 import cz.muni.fi.pv168.project.business.model.Template;
 import cz.muni.fi.pv168.project.business.service.validation.common.StringLengthValidator;
 
