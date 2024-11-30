c5a5df01edb9c2117f3467ad5aee563078bfc1b8
Maroš Pavlík <xpavlik9@fi.muni.cz>
2024-11-30 12:01:10 +0100

    ADD - TemplateDao complete

--- src/main/java/cz/muni/fi/pv168/project/storage/sql/dao/TemplateDao.java
+++ src/main/java/cz/muni/fi/pv168/project/storage/sql/dao/TemplateDao.java
@@ -1,11 +1,17 @@
 package cz.muni.fi.pv168.project.storage.sql.dao;
 
+import cz.muni.fi.pv168.project.business.model.Status;
 import cz.muni.fi.pv168.project.storage.sql.db.ConnectionHandler;
+import cz.muni.fi.pv168.project.storage.sql.entity.TaskEntity;
 import cz.muni.fi.pv168.project.storage.sql.entity.TemplateEntity;
 
+import java.sql.Date;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -71,26 +77,150 @@ public class TemplateDao implements DataAccessObject<TemplateEntity> {
 
     @Override
     public Collection<TemplateEntity> findAll() {
-        return null;
+        var sql = """
+                SELECT description,
+                    categoryId,
+                    templateName,
+                    taskName,
+                    assignedTo,
+                    allocatedTime,
+                    timeUnitId
+                FROM Template
+                """;
+        try (
+                var connection = connections.get();
+                var statement = connection.use().prepareStatement(sql)
+        ) {
+
+            List<TemplateEntity> templates = new ArrayList<>();
+            try (var resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    var template = templateFromResultSet(resultSet);
+                    templates.add(template);
+                }
+            }
+
+            return templates;
+        } catch (SQLException ex) {
+            throw new DataStorageException("Failed to load all templates", ex);
+        }
     }
 
     @Override
     public Optional<TemplateEntity> findById(Long id) {
-        return Optional.empty();
+        var sql = """
+                SELECT description,
+                    categoryId,
+                    templateName,
+                    taskName,
+                    assignedTo,
+                    allocatedTime,
+                    timeUnitId
+                FROM Template
+                WHERE id = ?
+                """;
+        try (
+                var connection = connections.get();
+                var statement = connection.use().prepareStatement(sql)
+        ) {
+            statement.setLong(1, id);
+            var resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return Optional.of(templateFromResultSet(resultSet));
+            } else {
+                // template not found
+                return Optional.empty();
+            }
+        } catch (SQLException ex) {
+            throw new DataStorageException("Failed to load template by id", ex);
+        }
     }
 
     @Override
     public TemplateEntity update(TemplateEntity entity) {
-        return null;
+        var sql = """
+                UPDATE Template
+                SET description = ?,
+                    categoryId = ?,
+                    templateName = ?,
+                    taskName = ?,
+                    assignedTo = ?,
+                    allocatedTime = ?,
+                    timeUnitId = ?
+                WHERE id = ?
+                """;
+        try (
+                var connection = connections.get();
+                var statement = connection.use().prepareStatement(sql)
+        ) {
+            statement.setString(1, entity.description());
+            statement.setLong(2, entity.categoryId());
+            statement.setString(3, entity.templateName());
+            statement.setString(4, entity.taskName());
+            statement.setString(5, entity.assignedTo());
+            statement.setInt(6, entity.allocatedTime());
+            statement.setLong(7, entity.timeUnitId());
+            statement.setLong(8, entity.id());
+            statement.executeUpdate();
+
+            int rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated == 0) {
+                throw new DataStorageException("Template not found, id: " + entity.id());
+            }
+            if (rowsUpdated > 1) {
+                throw new DataStorageException("More then 1 template (rows=%d) has been updated: %s"
+                        .formatted(rowsUpdated, entity));
+            }
+            return entity;
+        } catch (SQLException ex) {
+            throw new DataStorageException("Failed to update template: " + entity, ex);
+        }
     }
 
     @Override
     public void deleteById(Long id) {
-
+        var sql = "DELETE FROM Template WHERE id = ?";
+        try (
+                var connection = connections.get();
+                var statement = connection.use().prepareStatement(sql)
+        ) {
+            statement.setLong(1, id);
+            int rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated == 0) {
+                throw new DataStorageException("Template not found, id: " + id);
+            }
+            if (rowsUpdated > 1) {
+                throw new DataStorageException("More then 1 template (rows=%d) has been deleted: %s"
+                        .formatted(rowsUpdated, id));
+            }
+        } catch (SQLException ex) {
+            throw new DataStorageException("Failed to delete template, id: " + id, ex);
+        }
     }
 
     @Override
     public void deleteAll() {
+        var sql = "DELETE FROM Template";
+        try (
+                var connection = connections.get();
+                var statement = connection.use().prepareStatement(sql)
+        ) {
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataStorageException("Failed to delete all templates", ex);
+        }
+    }
 
+    private static TemplateEntity templateFromResultSet(ResultSet resultSet) throws SQLException {
+        return new TemplateEntity(
+                resultSet.getLong("id"),
+                resultSet.getString("description"),
+                resultSet.getLong("categoryId"),
+                resultSet.getString("templateName"),
+                resultSet.getString("taskName"),
+                resultSet.getString("assignedTo"),
+                resultSet.getInt("allocatedTime"),
+                resultSet.getLong("timeUnitId")
+        );
     }
 }
