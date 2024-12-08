c4c9474b294f13c3e20a4034105ff2746dbd6fe3
Vladimír Borek <xborek@fi.muni.cz>
2024-12-08 19:37:50 +0100

    fix: add new template

--- src/main/java/cz/muni/fi/pv168/project/storage/sql/entity/mapper/TemplateMapper.java
+++ src/main/java/cz/muni/fi/pv168/project/storage/sql/entity/mapper/TemplateMapper.java
@@ -11,6 +11,7 @@ import cz.muni.fi.pv168.project.util.Constants;
 
 /**
  * Mapper from the {@link TemplateEntity} to {@link Template}.
+ *
  * @author Maroš Pavlík
  */
 public class TemplateMapper implements EntityMapper<TemplateEntity, Template> {
